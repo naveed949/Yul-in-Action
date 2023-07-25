1c8decbd01cff0edd0d7b91160f1bc7a3d2664d2
Naveed Iqbal <naveediqbal949@gmail.com>
2023-07-25 17:18:46 +0000

    Operations doc

--- src/Operations.sol
+++ src/Operations.sol
@@ -1,8 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity 0.8.17;
 
+/**
+ * @title Operations
+ * @dev This contract contains functions for performing various mathematical operations in Yul.
+ */
 contract Operations {
+    /**
+     * @dev Determines whether a given number is prime.
+     * @param num The number to check.
+     * @return ret True if the number is prime, false otherwise.
+     */
     function isPrime(uint256 num) public pure returns (bool ret) {
         uint256 half;
 
@@ -19,9 +27,7 @@ contract Operations {
             }
             for {
                 let i := 2
-            } 
-            // loop until i < half
-            lt(i, half) {
+            } lt(i, half) { // loop until i < half
                 // increment i by 1
                 i := add(i, 1)
             } {
@@ -35,29 +41,35 @@ contract Operations {
         }
     }
 
+    /**
+     * @dev Returns the maximum value in an array of uint256 values.
+     * @param nums The array of values to search.
+     * @return max The maximum value in the array.
+     * @notice array slot is 0
+     * array length is stored in first 32 bytes
+     * array elements start at 32 bytes
+     * nums[0] can loaded by mload(add(nums, 0x20))
+     * nums[1] can loaded by mload(add(nums, 0x40))
+     * nums[2] can loaded by mload(add(nums, 0x60))
+     * and so on...
+     */
     function maxNum(uint256[] memory nums) public pure returns (uint256 max) {
-       /** 
-         * array slot is 0
-         * array length is stored in first 32 bytes
-         * array elements start at 32 bytes
-         * nums[0] can loaded by mload(add(nums, 0x20))
-         * nums[1] can loaded by mload(add(nums, 0x40))
-         * nums[2] can loaded by mload(add(nums, 0x60))
-         * and so on...
-         */
         assembly {
             // loading nums[0] into max - skipped first 32 bytes (length of array)
             max := mload(add(nums, 0x20))
             // index of 2nd element is 2 * 32 = 0x40
             let i := 2
             let j
-            for { } 
-            lt(i, add(mload(nums),1) ) // loop until i < length of array + 1
-            { i := add(i, 1) } // increment i by 1
-            {
+            for {
+
+            } lt(i, add(mload(nums), 1)) {
+                // loop until i < length of array + 1
+                i := add(i, 1)
+            } {
+                // increment i by 1
                 // load nums[i] into j
                 j := mload(add(nums, mul(i, 0x20)))
-                // if j > max, set max to j 
+                // if j > max, set max to j
                 if gt(j, max) {
                     max := j
                 }
